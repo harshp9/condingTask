a9e060851a5ce440d9c4e9826195b0b6ab4001dd
Harsh Patel <harpatel@redhat.com>
2021-02-16 13:37:15 -0500

    task 2 added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,10 +7,48 @@ fun total(a: Int, b: Double, c: Int, d: Double) = (a * b) + (c * d)
 
 fun main(args: Array<String>) {
     println("How many apples do you need?")
-    val apples = Integer.valueOf(readLine())
+    var apples = Integer.valueOf(readLine())
     println("How many oranges do you need?")
-    val oranges = Integer.valueOf(readLine())
+    var oranges = Integer.valueOf(readLine())
     println("You want $apples apples and $oranges oranges")
+
+    if (apples > 1 && apples % 2 == 0){
+            apples /= 2
+    }
+    else if (apples > 1 && apples % 2 != 0){
+        apples -= 1
+        apples /= 2
+        apples += 1
+    }
+
+    if (oranges >= 3 && oranges % 3 == 0){
+        oranges /= 3
+        oranges *= 2
+        //what about the rest if you bought 10 should be 6+1 = 7
+        //works only at exact
+        //needs exception when its more than a multiple of 3 and there is room to use discount
+    }
+    else if (oranges >= 3 && oranges % 3 == 1){
+        oranges -= 1
+
+        oranges /= 3
+        oranges *= 2
+
+        oranges += 1
+    }
+    else if (oranges >= 3 && oranges % 3 == 2){
+        oranges -= 2
+
+        oranges /= 3
+        oranges *= 2
+
+        oranges += 2
+    }
+
+    println("$apples")
+    println("$oranges")
+
+
     val costApples = 0.60
     val costOranges = 0.25
 
---
+++ src/test/kotlin/testTask.kt
@@ -0,0 +1,14 @@
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class testTask {
+//    @Test
+//    fun returnsNumberOfApples() {
+//        assertEquals(“How many apples do you need”, helloWorld(“Molly”))
+//    }
+////    @Test
+////    fun returnsNumberOfOranges() {
+////        assertEquals(“Hello, Molly!”, helloWorld(“Molly”))
+////    }
+
+}
